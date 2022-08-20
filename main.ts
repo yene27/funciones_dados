4ac17a38e3f9bceb872fefb703de54f06052f19b
yene27 <yene27@yahoo.com>
2022-08-20 14:14:30 -0500

    ajustado para usar con todos los botones

--- main.ts
+++ main.ts
@@ -47,8 +47,17 @@ function setDicePatterns () {
 function rollDice () {
     lista[randint(0, 5)].showImage(0)
 }
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    music.playSoundEffect(music.builtinSoundEffect(soundExpression.spring), SoundExpressionPlayMode.UntilDone)
+})
+input.onButtonPressed(Button.A, function () {
+    rollDice()
+})
 input.onGesture(Gesture.Shake, function () {
     rollDice()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(randint(1, 6))
 })
 let lista: Image[] = []
 setDicePatterns()
